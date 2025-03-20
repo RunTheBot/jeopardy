a4b41cc6062325601e5d67d917e871ab619c759a
RunTheBot <58890327+RunTheBot@users.noreply.github.com>
2025-03-20 14:52:38 -0400

    refactor main game layout; adjust button dimensions based on screen size

--- core/src/main/java/me/runthebot/jeopardy/Main.java
+++ core/src/main/java/me/runthebot/jeopardy/Main.java
@@ -19,15 +19,15 @@ public class Main extends ApplicationAdapter {
         VisUI.load(SkinScale.X1);
         stage = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(stage);
-        
+
         // Create a grid with categories and questions
         com.kotcrab.vis.ui.widget.VisTable mainTable = new com.kotcrab.vis.ui.widget.VisTable();
         mainTable.setFillParent(true);
-        
+
         // Categories
         String[] categories = {"Science", "History", "Sports", "Movies", "Geography"};
         int[] values = {200, 400, 600, 800, 1000};
-        
+
         // Add categories row
         for (String category : categories) {
             com.kotcrab.vis.ui.widget.VisLabel categoryLabel = new com.kotcrab.vis.ui.widget.VisLabel(category);
@@ -35,24 +35,25 @@ public class Main extends ApplicationAdapter {
             mainTable.add(categoryLabel).pad(10).width(120).height(50).fill();
         }
         mainTable.row();
-        
+
         // Add questions
         for (int value : values) {
             for (int i = 0; i < categories.length; i++) {
-            final com.kotcrab.vis.ui.widget.VisTextButton button = new com.kotcrab.vis.ui.widget.VisTextButton("$" + value);
-            button.setFocusBorderEnabled(false);
-            button.addListener(new com.badlogic.gdx.scenes.scene2d.utils.ClickListener() {
-                @Override
-                public void clicked(com.badlogic.gdx.scenes.scene2d.InputEvent event, float x, float y) {
-                button.setText(""); // When clicked, hide the value
-                button.setDisabled(true);
-                }
-            });
-            mainTable.add(button).pad(5).width(120).height(80).fill();
+                final com.kotcrab.vis.ui.widget.VisTextButton button = new com.kotcrab.vis.ui.widget.VisTextButton("$" + value);
+                button.setFocusBorderEnabled(false);
+                button.addListener(new com.badlogic.gdx.scenes.scene2d.utils.ClickListener() {
+                    @Override
+                    public void clicked(com.badlogic.gdx.scenes.scene2d.InputEvent event, float x, float y) {
+                    button.setText(""); // When clicked, hide the value
+                    button.setDisabled(true);
+                    }
+                });
+                float screenWidth = Gdx.graphics.getWidth();
+                float screenHeight = Gdx.graphics.getHeight();
+                mainTable.add(button).pad(5).width(screenWidth * 0.15f).height(screenHeight * 0.1f).fill();
             }
             mainTable.row();
         }
-        
         stage.addActor(mainTable);
     }
 
